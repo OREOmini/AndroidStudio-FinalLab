546d52646701babd71c2b02494f237fe91142e68
OREOmini <brendawangwangwang@gmail.com>
2016-12-07 20:40:25 +0800

    timer

--- app/src/main/java/com/example/wangyunwen/finallab/ItemDetails.java
+++ app/src/main/java/com/example/wangyunwen/finallab/ItemDetails.java
@@ -1,29 +1,33 @@
 package com.example.wangyunwen.finallab;
 
-import android.annotation.TargetApi;
-import android.icu.text.SimpleDateFormat;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class ItemDetails extends AppCompatActivity {
     TextView timer, content;
     int year, month, day, hour, minute, second;
+    String con;
+    long leftTime;
     private MyTimer myTimer;
 
     public void putTest() {
-        year = 0;
-        month = 2;
-        day = 3;
-        hour = 13;
-        minute = 32;
+        year = 2016;
+        month = 12;
+        day = 6;
+        hour = 23;
+        minute = 30;
         second = 60;
+        con = "homework";
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,27 +37,62 @@ public class ItemDetails extends AppCompatActivity {
         putTest();
 
         timer = (TextView) findViewById(R.id.timer);
-        long diff = 0;
+        content = (TextView) findViewById(R.id.content);
+        content.setText(con);
+
         try {
-            diff = calculateRemainTime();
+            leftTime = calculateRemainTime();
         } catch (ParseException e) {
             e.printStackTrace();
         }
-        myTimer = new MyTimer(diff, 1000);
+        myTimer = new MyTimer(leftTime, 1000);
+        Log.i("------", leftTime+"");
         myTimer.start();
     }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        myTimer.cancel();
+    }
 
-    @TargetApi(Build.VERSION_CODES.N)
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        myTimer.cancel();
+    }
+
     private long calculateRemainTime() throws ParseException {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        Log.i("expDate", day+"");
         String date = year + "-" + month + "-" + day + " " + hour + ":" + minute;
         Date curDate = new Date(System.currentTimeMillis());
         Date expDate = dateFormat.parse(date);
-        long diff = expDate.getTime() - curDate.getTime();
+        Log.i("expDate", expDate.toString());
+        Log.i("curDate", curDate.toString());
+
+        Long diff = expDate.getTime() - curDate.getTime();
+
         return diff;
     }
 
     public class MyTimer extends CountDownTimer {
+        int year, month, day, hour, minute, second;
+        CountDownTimer countDownTimer;
+
+        public void setExpDate(Date date) {
+            try {
+                leftTime = calculateRemainTime();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+
+
         public MyTimer(long millisInFuture, long countDownInterval) {
             super(millisInFuture, countDownInterval);
         }
@@ -63,42 +102,61 @@ public class ItemDetails extends AppCompatActivity {
             timer.setText("done");
         }
 
-        @TargetApi(Build.VERSION_CODES.N)
         @Override
         public void onTick(long millisUntilFinished) {
             //Log.i("MainActivity", millisUntilFinished + "");
-            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            if(second-1 >= 0) {
-                second--;
-            } else {
-                second = 59;
-                if(minute-1 >= 0) {
-                    minute--;
-                } else {
-                    minute = 59;
-                    if(hour-1 >= 0) {
-                        hour--;
-                    } else {
-                        hour = 23;
-                        if(day-1 > 0) {
-                            day--;
-                        } else {
-                            day = 30;
-                            if (month-1 > 0) {
-                                month--;
-                            } else {
-                                month = 12;
-                                year--;
-                            }
-                        }
-                    }
-                }
-            }
 
-            String time = year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
+            leftTime -= 1000;
+
+            String formatType = "HH时mm分ss秒";
+            Date tempdate = new Date(millisUntilFinished);
+            SimpleDateFormat format = new SimpleDateFormat(formatType);
+            format.setTimeZone(TimeZone.getTimeZone("GMT+00:00"));
+            String time = (tempdate.getYear()-70) + "年" + (tempdate.getMonth()) + "月" +
+                    (tempdate.getDay()-4) + "日"+format.format(millisUntilFinished);
+            //Log.i("----", tempdate.toString());
+
+            //Log.i("------", tempdate.getYear()+" "+tempdate.getMonth()+" "+tempdate.getDay());
+            //String time = new SimpleDateFormat(formatType).format(date);
+            //String time = (tempdate.getYear()- 70) + "年" + (tempdate.getMonth()) + "月" +
+                    //(tempdate.getDay()-4) + "日"+ (tempdate.getHours() - 8) + new SimpleDateFormat(formatType).format(tempdate);
+//            int y, m, d, h, mi, s;
+//            y = (int ) (millisUntilFinished / (31536000*1000));
+//            m = (int)
+
+//            if(second-1 >= 0) {
+//                second--;
+//            } else {
+//                second = 59;
+//                if(minute-1 >= 0) {
+//                    minute--;
+//                } else {
+//                    minute = 59;
+//                    if(hour-1 >= 0) {
+//                        hour--;
+//                    } else {
+//                        hour = 23;
+//                        if(day-1 > 0) {
+//                            day--;
+//                        } else {
+//                            day = 30;
+//                            if (month-1 > 0) {
+//                                month--;
+//                            } else {
+//                                month = 12;
+//                                year--;
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//
+//            String time = year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
 
             timer.setText(time);
         }
+
+
     }
 
 }
